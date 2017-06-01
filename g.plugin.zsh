c80f84ea5e3f12b60a312c95e3920599d4d95d01
Attila Gonda <pcdevil@users.noreply.github.com>
2017-06-01 16:07:50 +0200

    feat(g): add 'set-user' command

--- g.plugin.zsh
+++ g.plugin.zsh
@@ -16,6 +16,10 @@ function git {
 			shift
 			git-cd "$@"
 			;;
+		su|set-user)
+			shift
+			git-set-user "$@"
+			;;
 		ta|take)
 			shift
 			git-take "$@"
@@ -34,14 +38,72 @@ function git-cd {
 	return $?
 }
 
+function git-set-user {
+	local repoUrl=$1
+	local shortRepoUrl=$(_get-short-url $repoUrl)
+
+	local name=$(_get-git-user-var name $shortRepoUrl)
+	local email=$(_get-git-user-var email $shortRepoUrl)
+
+	git config user.name "$name"
+	git config user.email "$email"
+}
+
 function git-take {
 	local repository="$1"
 	local directory=${2:-$(basename $repository .git)}
 
 	git clone --recurse-submodules $repository $directory &&
 		cd $directory &&
-		git set-user $repository
+		git-set-user $repository
 	return $?
+}
+
+function _get-git-user-var {
+	local variable="$1"
+	local url="$2"
+
+	git config --get-urlmatch user.$variable $url 2>/dev/null ||
+		git config user.$variable
+}
+
+function _get-short-url {
+	local domain="$1"
+
+	if [[ -z $domain ]]; then
+		return
+	fi
+
+	# remove anything before @ (deals with git@ and other username/passwords)
+	domain=${domain#*@}
+
+	# save and remove protocol
+	local protocol=${domain%%://*}
+	if [[ ${#protocol} == ${#domain} ]]; then
+		protocol=https
+	fi
+	protocol=${protocol}://
+	domain=${domain#*://}
+
+	# remove port (and username if git@ url)
+	domain=${domain%%:*}
+
+	# remove pathname
+	domain=${domain%%/*}
+
+	# save and remove tld
+	local tld=${domain##*.}
+	if [[ ${#tld} == ${#domain} ]]; then
+		tld=""
+	else
+		tld=.${tld}
+	fi
+	domain=${domain%.*}
+
+	# remove subdomains
+	domain=${domain##*.}
+
+	echo $protocol$domain$tld
 }
 
 unalias g
